55fa3be710a86e73a9cf6bbbac054532cfa8a238
kotanishikanth <kotanishikanth@outlook.com>
2020-06-01 19:20:29 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { AppTableComponent } from './app-table/app-table.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, AppTableComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
