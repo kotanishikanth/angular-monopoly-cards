6091e28da0b3356b32c46a5061dc28bb37ab419b
kotanishikanth <kotanishikanth@outlook.com>
2020-06-01 19:40:55 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { TableComponent } from './table/table.component';
+import { TableService } from './table.service';
 import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, TableComponent, PlayerComponent],
   bootstrap: [AppComponent],
-  providers: []
+  providers: [TableService]
 })
 export class AppModule {}
 
