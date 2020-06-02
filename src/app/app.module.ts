83b39b8dfd292f174725fa472c243ead34ae2bfe
kotanishikanth <kotanishikanth@outlook.com>
2020-06-01 20:02:49 -0400

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from "./app.component";
 import { TableComponent } from './table/table.component';
 import { TableService } from './table.service';
 import { PlayerComponent } from './player/player.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, TableComponent, PlayerComponent],
+  declarations: [AppComponent, TableComponent, PlayerComponent, CardComponent],
   bootstrap: [AppComponent],
   providers: [TableService]
 })
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -7,10 +7,17 @@ import { Table } from '../table';
   templateUrl: './table.component.html',
   styleUrls: ['../app.component.css', './table.component.css']
 })
+
 export class TableComponent implements OnInit {
 
   tableId:number = 0;
   table: Table;
+
+  get hostPlayer(){
+    let hostPlayer = this.table.players.find(x => x.id == this.table.hostid);
+    if(hostPlayer) return hostPlayer;
+    else return {name: "System"};
+  }
 
   constructor(private tableService: TableService ) { }
 
